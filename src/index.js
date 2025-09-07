694cc86f88fcf843ab2d1cca6c8762be0b59f8da
Laux0 <laura.rahmati@gmail.com>
2025-09-07 22:11:03 +0200

    Set time to update every second

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-let londonElement = document.querySelector("#london");
-let londonDateElement = londonElement.querySelector(".city-date");
-let londonTimeElement = londonElement.querySelector(".city-time");
-let londonTime = moment().tz("Europe/London");
-londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
-londonTimeElement.innerHTML = londonTime.format("hh:mm:ss[<small>]A[</small>]");
+function displayCityTime() {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".city-date");
+  let londonTimeElement = londonElement.querySelector(".city-time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "hh:mm:ss[<small>]A[</small>]"
+  );
 
-let osloElement = document.querySelector("#oslo");
-let osloDateElement = osloElement.querySelector(".city-date");
-let osloTimeElement = osloElement.querySelector(".city-time");
-let osloTime = moment().tz("Europe/Oslo");
-osloDateElement.innerHTML = moment().format("MMMM Do YYYY");
-osloTimeElement.innerHTML = osloTime.format("hh:mm:ss[<small>]A[</small>]");
+  let osloElement = document.querySelector("#oslo");
+  let osloDateElement = osloElement.querySelector(".city-date");
+  let osloTimeElement = osloElement.querySelector(".city-time");
+  let osloTime = moment().tz("Europe/Oslo");
+  osloDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  osloTimeElement.innerHTML = osloTime.format("hh:mm:ss[<small>]A[</small>]");
+}
+setInterval(displayCityTime, 1000);

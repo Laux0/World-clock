e4a57f62b5f0bb7602ef91859515b54856fc85e2
Laux0 <laura.rahmati@gmail.com>
2025-09-07 20:37:04 +0200

    Used moment timezone to access the date and time in each city

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 let londonElement = document.querySelector("#london");
 let londonDateElement = londonElement.querySelector(".city-date");
 let londonTimeElement = londonElement.querySelector(".city-time");
+let londonTime = moment().tz("Europe/London");
 londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
-londonTimeElement.innerHTML = "2:40:27<small>AM</small>";
+londonTimeElement.innerHTML = londonTime.format("hh:mm:ss[<small>]A[</small>]");
 
 let osloElement = document.querySelector("#oslo");
 let osloDateElement = osloElement.querySelector(".city-date");
 let osloTimeElement = osloElement.querySelector(".city-time");
+let osloTime = moment().tz("Europe/Oslo");
 osloDateElement.innerHTML = moment().format("MMMM Do YYYY");
-osloTimeElement.innerHTML = "2:40:27<small>AM</small>";
+osloTimeElement.innerHTML = osloTime.format("hh:mm:ss[<small>]A[</small>]");

00a2295ed7a8cd43733b0ca8494c8254ecf8d9f4
Laux0 <laura.rahmati@gmail.com>
2025-09-07 17:21:56 +0200

    Targeted Date Element for the cities from JavaScript

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let londonElement = document.querySelector("#london");
+let londonDateElement = londonElement.querySelector(".city-date");
+londonDateElement.innerHTML = "August 15th 2025";
+
+let osloElement = document.querySelector("#oslo");
+let osloDateElement = osloElement.querySelector(".city-date");
+osloDateElement.innerHTML = "August 15th 2025";

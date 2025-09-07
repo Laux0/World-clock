83a3125fb9988f9ac4d6440f92f6f4d53bd9c1e7
Laux0 <laura.rahmati@gmail.com>
2025-09-07 17:26:34 +0200

    Targeted the city time elements from JavaScript

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 let londonElement = document.querySelector("#london");
 let londonDateElement = londonElement.querySelector(".city-date");
+let londonTimeElement = londonElement.querySelector(".city-time");
 londonDateElement.innerHTML = "August 15th 2025";
+londonTimeElement.innerHTML = "2:40:27AM";
 
 let osloElement = document.querySelector("#oslo");
 let osloDateElement = osloElement.querySelector(".city-date");
+let osloTimeElement = osloElement.querySelector(".city-time");
 osloDateElement.innerHTML = "August 15th 2025";
+osloTimeElement.innerHTML = "2:40:27AM";

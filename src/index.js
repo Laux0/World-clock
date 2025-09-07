968d6cfeae7fb9e63c8aae21364cd8e1a2c1f544
Laux0 <laura.rahmati@gmail.com>
2025-09-07 23:40:11 +0200

    Update JavaScript to run only if elements exist on the page

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 function displayCityTime() {
   let londonElement = document.querySelector("#london");
-  let londonDateElement = londonElement.querySelector(".city-date");
-  let londonTimeElement = londonElement.querySelector(".city-time");
-  let londonTime = moment().tz("Europe/London");
-  londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  londonTimeElement.innerHTML = londonTime.format(
-    "hh:mm:ss[<small>]A[</small>]"
-  );
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".city-date");
+    let londonTimeElement = londonElement.querySelector(".city-time");
+    let londonTime = moment().tz("Europe/London");
+    londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
+    londonTimeElement.innerHTML = londonTime.format(
+      "hh:mm:ss[<small>]A[</small>]"
+    );
+  }
 
   let osloElement = document.querySelector("#oslo");
-  let osloDateElement = osloElement.querySelector(".city-date");
-  let osloTimeElement = osloElement.querySelector(".city-time");
-  let osloTime = moment().tz("Europe/Oslo");
-  osloDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  osloTimeElement.innerHTML = osloTime.format("hh:mm:ss[<small>]A[</small>]");
+  if (osloElement) {
+    let osloDateElement = osloElement.querySelector(".city-date");
+    let osloTimeElement = osloElement.querySelector(".city-time");
+    let osloTime = moment().tz("Europe/Oslo");
+    osloDateElement.innerHTML = moment().format("MMMM Do YYYY");
+    osloTimeElement.innerHTML = osloTime.format("hh:mm:ss[<small>]A[</small>]");
+  }
 }
 setInterval(displayCityTime, 1000);
 
